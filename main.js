3c70e2ec47941b0e603ef125ec0ce7ab5827e189
Ibadulla <asadli.ibadulla@gmail.com>
2024-09-23 01:20:45 +0600

    add home page

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 import './src/scss/main.scss'
 import './src/js/header'
+import Splide from '@splidejs/splide'
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (document.querySelector('.js-main-slider')) {
+    new Splide('.js-main-slider', {
+      pagination: true,
+      arrows: false,
+      type: 'fade',
+      autoplay: true,
+    }).mount()
+  }
+})
